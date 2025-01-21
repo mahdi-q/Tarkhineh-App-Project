20a1a158ecfea0662ebc8f976b76084ca896915e
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-01-21 15:44:07 +0330

    Develope "AppLayout" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "./Pages/AppLayout";
+import Home from "./Pages/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
