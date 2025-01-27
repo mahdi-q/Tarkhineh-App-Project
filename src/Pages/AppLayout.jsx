c348c066bad1484700a070f8016062f5e0cbfe16
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-01-27 15:09:19 +0330

    Develope "Header" feature

--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../UI/Navbar";
 import Footer from "../UI/Footer";
-import Header from "../UI/Header";
 
 function AppLayout() {
   return (
     <div>
       <Navbar />
-
-      <Header />
 
       <Outlet />
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,43 @@
+import Header from "../UI/Header";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="h-screen">
+      <Header
+        slides={[
+          {
+            id: 1,
+            image: "/public/Images/header-image-5.webp",
+            text: "تجربه غذای سالم و گیاهی به سبک ترخینه",
+            buttonText: "سفارش آنلاین غذا",
+          },
+          {
+            id: 2,
+            image: "/public/Images/header-image-2.webp",
+            text: "غذای دلخواه شما با بهترین کیفیت",
+            buttonText: "اکنون سفارش دهید",
+          },
+          {
+            id: 3,
+            image: "/public/Images/header-image-3.webp",
+            text: "تنوعی بی‌نظیر از غذاهای گیاهی",
+            buttonText: "همین حالا سفارش دهید",
+          },
+          {
+            id: 4,
+            image: "/public/Images/header-image-4.webp",
+            text: "غذاهایی متناسب با رژیم شما",
+            buttonText: "سفارش دهید",
+          },
+          {
+            id: 5,
+            image: "/public/Images/header-image-1.webp",
+            text: "لذت طعمی فراموش‌نشدنی",
+            buttonText: "امروز امتحان کنید",
+          },
+        ]}
+      />
+    </div>
+  );
 }
 export default Home;
