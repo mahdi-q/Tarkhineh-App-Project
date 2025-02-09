58cef83259d45bc88add9cdfcc9bb0681ca85057
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-02-09 10:52:16 +0330

    Refactor "Navbar" and "Sidebar" components for improved structure

--- src/Features/Navbar/Navbar.jsx
+++ src/Features/Navbar/Navbar.jsx
@@ -2,23 +2,12 @@ import NavbarBtns from "./NavbarBtns";
 import NavbarLinks from "./NavbarLinks";
 import Sidebar from "./Sidebar";
 import { Link } from "react-router-dom";
-import { useToggle } from "../../Contexts/ToggleContext";
 
 function Navbar() {
-  const { isToggleOpen } = useToggle();
-
   return (
     <div className="sticky top-0 z-10 border-b border-b-gray-200 bg-white bg-opacity-90 py-4 shadow-sm lg:px-16">
       <div className="container flex flex-row items-center justify-between">
-        <div
-          className={
-            isToggleOpen
-              ? "fixed left-0 top-0 h-screen w-full bg-gray-600/30 backdrop-blur-sm md:hidden"
-              : "md:hidden"
-          }
-        >
-          <Sidebar />
-        </div>
+        <Sidebar />
 
         <Link to="/">
           <img
